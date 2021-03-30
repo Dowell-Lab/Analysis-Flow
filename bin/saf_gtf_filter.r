6719c958f0c22cc57c3fb1fb9b69d57929c72bf6
Zach Maas <zach.maas@gmail.com>
2021-03-30 10:49:27 -0600

    Add code to filter GTF by filtered isoform annotation

---
+++ bin/saf_gtf_filter.r
@@ -0,0 +1,55 @@
+### saf_gtf_filter.r --- Filter a GTF file using a SAF file
+##
+## Filename: saf_gtf_filter.r
+## Author: Zachary Maas <zama8258@colorado.edu>
+## Created: Mon Feb 15 08:45:09 2021 (-0700)
+##
+######################################################################
+##
+### Commentary:
+##
+## Filter a GTF file using a SAF file. This exists to allow for proper
+## exon counting when analyzing RNA-seq data in the pipeline as
+## opposed to nascent.
+##
+######################################################################
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or (at
+## your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+##
+######################################################################
+##
+### Code:
+
+library('tidyverse')
+
+## Sample files (TODO switch to argparse)
+gtf <- "/hdd/data/dna_lab/nina/data/hg38_refseq.gtf"
+saf <- "/hdd/data/dna_lab/sequencing_analysis_utils/data/full.saf"
+out_gtf <- "/hdd/data/dna_lab/nina/data/hg38_refseq_isoforms.gtf"
+
+gtf_df <- read_delim(gtf, delim="\t", col_names=FALSE) %>%
+    separate(X9, c("n1", "geneid", "n2", "txid"), sep = " ") %>%
+    mutate(geneid = str_remove_all(geneid, '[;"]'))
+
+saf_df <- read_delim(saf, delim="\t", col_names=FALSE) %>%
+    mutate(geneid = X1)
+
+filtered_df <- gtf_df %>% semi_join(saf_df, by=c("geneid")) %>%
+    mutate(geneid=paste0('"', geneid, '";')) %>%
+    unite(metadata, n1, geneid, n2, txid, sep=" ")
+
+write_tsv(filtered_df, out_gtf, col_names = FALSE)
+
+######################################################################
+### saf_gtf_filter.r ends here
